3c057ea472edb2969df1b832e4b0bed503d58b25
Connor Stack <connorstack123@gmail.com>
2025-08-16 23:19:12 -0500

    Format change

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,48 +9,33 @@ group = "personal.project"
 version = "0.0.1-SNAPSHOT"
 description = "Demo project for Spring Boot"
 
+val netflixDgsVersion: String by extra("10.2.1")
+val flywayVersion: String by extra("11.10.0")
+
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(17)
 	}
 }
-
 repositories {
 	mavenCentral()
 }
 
-//extra["netflixDgsVersion"] = "10.2.1"
-//extra["flywayVersion"] = "11.10.0"
-val netflixDgsVersion: String by extra("10.2.1")
-val flywayVersion: String by extra("11.10.0")
-
 dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-actuator")
-//	implementation("org.springframework.boot:spring-boot-starter-web")
-//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-//	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
-//	implementation("org.jetbrains.kotlin:kotlin-reflect")
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.flywaydb:flyway-core:${extra["flywayVersion"]}")
-//	implementation("org.flywaydb:flyway-database-postgresql:${extra["flywayVersion"]}")
-//	testImplementation("org.springframework.boot:spring-boot-starter-test")
-//	testImplementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter-test")
-//	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-//	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-//	runtimeOnly("org.postgresql:postgresql")
-
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+	// jpa
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-	// ✅ Flyway core + Postgres plugin (same version)
+	// flyway
 	implementation("org.flywaydb:flyway-core:$flywayVersion")
 	implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
 
+	// postgres
 	runtimeOnly("org.postgresql:postgresql")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
